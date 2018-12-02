3909c08fddd06b915cf3fa5ffc4d04e19050a951
Donovan Rost <drost@ucsc.edu>
2018-12-01 18:03:06 -0800

    firebase inited

--- word-ass/app/main.js
+++ word-ass/app/main.js
@@ -5,7 +5,21 @@ import VueDevtools from 'nativescript-vue-devtools'
 import Vuex from 'vuex'
 import { store } from './store/index.js'
 
+var firebase = require("nativescript-plugin-firebase");
+
 Vue.use(Vuex)
+
+firebase.init({
+  // Optionally pass in properties for database, authentication and cloud messaging,
+  // see their respective docs.
+}).then(
+    function (instance) {
+      console.log("firebase.init done");
+    },
+    function (error) {
+      console.log("firebase.init error: " + error);
+    }
+)
 
 
 if(TNS_ENV !== 'production') {
